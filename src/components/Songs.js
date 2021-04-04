8783c37552e2aec6283a300abe7ff43e85847f6d
Trayshawn <traytwebb14@yahoo.com>
2021-04-04 00:32:54 -0500

    created a songs component for songs list/playlist

---
+++ src/components/Songs.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Songs extends Component {
+    render() {
+        return (
+            <div>
+                Songs
+                
+            </div>
+        )
+    }
+}
+
+export default Songs
