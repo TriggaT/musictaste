cb46d2708d706410c1f09215ae5a68442d4702ad
Trayshawn <traytwebb14@yahoo.com>
2021-04-04 00:46:44 -0500

    create songform component to create new songs

---
+++ src/components/SongForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SongForm extends Component {
+    render() {
+        return (
+            <div>
+                SongForm
+            </div>
+        )
+    }
+}
+
+export default SongForm
