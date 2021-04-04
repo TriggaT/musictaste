e3394f8bf2dec24204f017c77a33d7a93ce63cde
Trayshawn <traytwebb14@yahoo.com>
2021-04-03 23:42:42 -0500

    converted app function into app class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <h1>MusicTaste</h1>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>MusicTaste</h1>
+      </div>
+    );
+
+  }
+  
 }
 
 export default App;
