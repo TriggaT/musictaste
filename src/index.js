631825c712de7e51e9788248922b0b853bf55d63
Trayshawn <traytwebb14@yahoo.com>
2021-04-03 23:49:48 -0500

    added provider and store  with redux devtools to index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import reportWebVitals from './reportWebVitals';
+
+const store = createStore(manageSongs, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
