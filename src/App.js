b4ce40570d4e9d237407a6f6d0f2fb2b5221d919
Trayshawn <traytwebb14@yahoo.com>
2021-04-04 00:23:09 -0500

    created a home component for home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, {Component} from "react"
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import NavBar from './components/NavBar'
 
 class App extends Component {
   render(){
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom';
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                NavBar
+            </div>
+        )
+    }
+}
+
+export default NavBar
