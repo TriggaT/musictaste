ab9dad641f2fbe133844b76111c8cf422da11539
Trayshawn <traytwebb14@yahoo.com>
2021-04-05 11:52:05 -0500

    added placeholder lifecycle event and list songs info in songs

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -3,10 +3,24 @@ import { NavLink } from 'react-router-dom';
 import {connect} from 'react-redux'
 
 class Songs extends Component {
+    componentDidMount = () => {
+        console.log(this.props.songs)
+    }
+
+
+
     render() {
         return (
             <div>
                 <h3>Songs</h3>
+                <ol>
+                    {this.props.songs.map(song => 
+                        <li key={song.title}>
+                            <strong>Title:</strong> {song.title} <br /> 
+                            <strong>Artist:</strong> {song.artist}
+                        </li>)} 
+                </ol>
+
                 <NavLink to="/songs/new">Add New Song</NavLink>
                 
             </div>
