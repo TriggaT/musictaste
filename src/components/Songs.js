92766c4575a6d672d62d135a9766f0a389e282b2
Trayshawn <traytwebb14@yahoo.com>
2021-04-05 11:31:42 -0500

    connected Songs component to store for access to state

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom';
+import {connect} from 'react-redux'
 
 class Songs extends Component {
     render() {
@@ -13,4 +14,4 @@ class Songs extends Component {
     }
 }
 
-export default Songs
+export default connect((state) => ({songs: state.songs}))(Songs)
