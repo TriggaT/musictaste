c8edf6f97d5ebde29d19d378ccd35e85d46dcddc
Trayshawn <traytwebb14@yahoo.com>
2021-04-05 14:10:02 -0500

    added histroy prop as an argument inside handleSubmit addSong prop function

--- src/components/SongForm.js
+++ src/components/SongForm.js
@@ -17,7 +17,7 @@ class SongForm extends Component {
 
     handleSubmit = e => {
         e.preventDefault()
-        this.props.addSong(this.state)
+        this.props.addSong(this.state, this.props.history)
         this.setState({
             title: "",
             artist: ""
