18febfa6127bd6e12bf751f3867238b294ea7f24
Trayshawn <traytwebb14@yahoo.com>
2021-04-05 09:53:03 -0500

    added link to song form into songs component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,6 @@ class NavBar extends Component {
     render() {
         return (
             <div>
-                NavBar
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="/songs">Songs</NavLink>
             </div>
--- src/components/Songs.js
+++ src/components/Songs.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom';
 
 class Songs extends Component {
     render() {
         return (
             <div>
-                Songs
+                <h3>Songs</h3>
+                <NavLink to="/songs/new">Add New Song</NavLink>
                 
             </div>
         )
