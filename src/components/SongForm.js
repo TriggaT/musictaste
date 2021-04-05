e8973d195e20df9b1fc6dbb7c48d4bf34a1c369c
Trayshawn <traytwebb14@yahoo.com>
2021-04-05 10:19:54 -0500

    added handle change function to song form component with values of state in  input

--- src/components/SongForm.js
+++ src/components/SongForm.js
@@ -7,15 +7,23 @@ class SongForm extends Component {
         artist: ""
     }
 
-    
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value 
+        })
+        console.log(this.state)
+
+    }
+
+
     render() {
         return (
             <div>
                 <form>
                     <label htmlFor="title">Title:</label>
-                    <input name="title" type="text"></input><br /> 
+                    <input name="title" type="text" value={this.state.title} /><br /> 
                     <label htmlFor="artist">Artist:</label>
-                    <input name="title" type="text"></input><br />
+                    <input name="title" type="text" value={this.state.title} /><br />
                     <input type="submit" value="Add Song" />
                 </form>
             </div>
