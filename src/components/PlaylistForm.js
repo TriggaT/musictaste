0f87bd7e681e91682db709529689c925a1d1135e
Trayshawn <traytwebb14@yahoo.com>
2021-04-06 11:03:19 -0500

    added form to playlistform component and fixed typo of with state of said component

--- src/components/PlaylistForm.js
+++ src/components/PlaylistForm.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react'
 
 class PlaylistForm extends Component {
     state = {
-        title: "",
-        artist: ""
+        name: "",
+        description: ""
     }
 
     render() {
         return (
             <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="name">Name:</label>
+                    <input name="name" type="text" value={this.state.name} onChange={this.handleChange}/><br /> 
+                    <label htmlFor="description">Description:</label>
+                    <input name="description" type="text" value={this.state.description} onChange={this.handleChange}/><br />
+                    <input type="submit" value="Add Song" />
+                </form>
                 
                 
             </div>
