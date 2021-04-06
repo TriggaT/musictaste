e83a5b5f8e7c172e324d4a703d89b3ece8abe82b
Trayshawn <traytwebb14@yahoo.com>
2021-04-06 14:05:24 -0500

    added render to songs container

--- src/containers/SongsContainer.js
+++ src/containers/SongsContainer.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
 
-export class SongsContainer extends Component {
+export default class SongsContainer extends Component {
+    
+
     render() {
+        const { songs, playlistId, match } = this.props; 
         return (
             <div>
+                
+             
+                <Songs songs={songs} /> 
+                <br /> 
+
+                
+                {/* <Route exact path={this.props.match.url} render={() => <h4>Choose any of the above playlist</h4>}/> */}
+                
+                
+                
+            
                 
             </div>
         )
     }
 }
 
-export default SongsContainer
+
