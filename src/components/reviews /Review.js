2f106f9fbdfe7469467e7726781377c9028ee72e
Trayshawn <traytwebb14@yahoo.com>
2021-04-06 18:13:28 -0500

    created review component and reviews folder for related components

---
+++ src/components/reviews /Review.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Review extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Review
