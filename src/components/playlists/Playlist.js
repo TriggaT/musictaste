c143119f5f7a222dd4ffcc6fa967a097dd5587ba
Trayshawn <traytwebb14@yahoo.com>
2021-04-06 17:40:14 -0500

    imported reviewscontainer and set necessary props for proper viewing of reviews in container

--- src/components/playlists/Playlist.js
+++ src/components/playlists/Playlist.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
 import SongsContainer from "../../containers/SongsContainer";
+import ReviewsContainer from "../../containers/ReviewsContainer";
 
 class Playlist extends Component {
     render() {
-        const {playlists, match, songs} = this.props;
+        const {playlists, match, songs, reviews} = this.props;
         const playlist = playlists.find(p => p.id === parseInt(match.params.playlistId));
-        
+
         const associatedSongs = songs.filter(song => song.playlist_id === playlist.id);
+        const associatedReviews = reviews.filter(review => review.playlist_id === playlist.id);
+
+        console.log(associatedReviews)
         
         return (
             <>
             <h3>{playlist.name}</h3>
             <h4>{playlist.description}</h4>
-            <SongsContainer songs={associatedSongs} playlistId={playlist.id} match={match}/>     
+            <SongsContainer songs={associatedSongs} playlistId={playlist.id} match={match}/> 
+            <ReviewsContainer reviews={associatedReviews} playlistId={playlist.id} match={match}/>
+
             </>
         )
     }
