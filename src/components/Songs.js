d6d360f7ae70ec25302937385a210453bcbf3720
Trayshawn <traytwebb14@yahoo.com>
2021-04-06 16:23:14 -0500

    cleaned up spacing and removed unneed imports from Songs component

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -1,30 +1,10 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom';
-import {connect} from 'react-redux'
-import {getSongs} from '../actions/index'
 import Song from "./Song"
 
 class Songs extends Component {
-    // componentDidMount = () => {
-    //     this.props.getSongs()
-    // }
-
-
-
     render() {
-        const { songs, playlistId, match } = this.props; 
-        console.log(match.url)
-
-        // if (this.props.loading){ 
-            
-        //     return (
-        //         <>
-        //         <h3>Songs</h3>
-        //         <h4>Loading...</h4>
-        //         </>
-
-        //     )
-        // }
+        const { songs, playlistId, match } = this.props;     
         return (
             <div>
                 <h3>Songs</h3>
@@ -32,19 +12,11 @@ class Songs extends Component {
                     {songs.map(song => 
                         <Song  key={song.id} song={song} />  )} 
                 </ol>
-
                 <NavLink to={`${match.url}/songs/new`}>Add New Song</NavLink>
                 
             </div>
         )
     }
 }
-
-// const mapStateToProps = (state) => {
-//     return {
-//         songs: state.songs,
-//         loading: state.loading
-//     }
-// }
 
 export default Songs
