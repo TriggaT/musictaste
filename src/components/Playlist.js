240bcc2da78171713e550c01ad09394c91aa5f11
Trayshawn <traytwebb14@yahoo.com>
2021-04-06 16:17:33 -0500

    removed large gaps between code in playlist componenet

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -5,29 +5,15 @@ import SongsContainer from "../containers/SongsContainer";
 
 class Playlist extends Component {
     render() {
-        const {playlists, match, songs} = this.props
-
-        const playlist = playlists.find(p => p.id === parseInt(match.params.playlistId))
+        const {playlists, match, songs} = this.props;
+        const playlist = playlists.find(p => p.id === parseInt(match.params.playlistId));
+        const associatedSongs = songs.filter(song => song.playlist_id === playlist.id);
         
-        const associatedSongs = songs.filter(song => song.playlist_id === playlist.id)
-
-
-
         return (
             <>
             <h3>{playlist.name}</h3>
             <h4>{playlist.description}</h4>
-            <SongsContainer songs={associatedSongs} playlistId={playlist.id} match={match}/> 
-
-            {/* <h3>Songs</h3>
-                <ol>
-                    {associatedSongs.map(song => 
-                        <Song  song={song} />  )
-                    } 
-                </ol>
-
-                <NavLink to={`playlists/${playlist.id}/songs/new`}>Add New Song</NavLink> */}
-                
+            <SongsContainer songs={associatedSongs} playlistId={playlist.id} match={match}/>     
             </>
         )
     }
