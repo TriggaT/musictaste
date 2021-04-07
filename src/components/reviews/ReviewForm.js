ea5891ffad59125d100375f90bac44dc371525c5
Trayshawn <traytwebb14@yahoo.com>
2021-04-06 21:31:13 -0500

    added form to review form component

--- src/components/reviews/ReviewForm.js
+++ src/components/reviews/ReviewForm.js
@@ -4,7 +4,13 @@ export class ReviewForm extends Component {
     render() {
         return (
             <div>
-                
+                <form >
+                    <label htmlFor="rating">Rating:</label>
+                    <input name="title" type="text" value={this.state.title} /><br /> 
+                    <label htmlFor="reviewText">Review:</label>
+                    <input name="reviewText" type="textarea" value={this.state.artist} onChange={this.handleChange}/><br />
+                    <input type="submit" value="Add Song" />
+                </form>
             </div>
         )
     }
