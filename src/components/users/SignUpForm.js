3cab605da1a26b5ad545ca17976544255d016401
Trayshawn <traytwebb14@yahoo.com>
2021-04-07 18:00:25 -0500

    added form and initial state  to signup form component

--- src/components/users/SignUpForm.js
+++ src/components/users/SignUpForm.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
 
 export class SignUpForm extends Component {
+    
+    state = {
+        username: "",
+        password: ""
+    }
+    
     render() {
         return (
             <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="username">Username:</label>
+                    <input name="username" type="text" value={this.state.username} onChange={this.handleChange}/><br /> 
+                    <label htmlFor="password">Password:</label>
+                    <input name="password" type="password" value={this.state.password} onChange={this.handleChange}/><br />
+                    <input type="submit" value="Sign Up" />
+                </form>
                 
             </div>
         )
