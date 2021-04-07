70aeb48e5c7ebc6c45ff02e7d2c42278b03aaf2e
Trayshawn <traytwebb14@yahoo.com>
2021-04-07 17:49:50 -0500

    created users folder and created signup form component inside of it

---
+++ src/components/users/SignUpForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class SignUpForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default SignUpForm
