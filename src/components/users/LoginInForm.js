2289f3f36908ab51655b177526a28918c1db15f0
Trayshawn <traytwebb14@yahoo.com>
2021-04-08 11:25:54 -0500

    copied like code from signup form into login form component

--- src/components/users/LoginInForm.js
+++ src/components/users/LoginInForm.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
 
 export class LoginInForm extends Component {
+     state = {
+        username: "",
+        password: ""
+    }
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        console.log(this.state)
+    }
+
     render() {
         return (
             <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="username">Username:</label>
+                    <input name="username" type="text" value={this.state.username} onChange={this.handleChange}/><br /> 
+                    <label htmlFor="password">Password:</label>
+                    <input name="password" type="password" value={this.state.password} onChange={this.handleChange}/><br />
+                    <input type="submit" value="Sign Up" />
+                </form>
                 
             </div>
         )
