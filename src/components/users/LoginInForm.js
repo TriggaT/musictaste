b5462fc09e178876ac2385e548eca9180f0682ea
Trayshawn <traytwebb14@yahoo.com>
2021-04-08 11:19:53 -0500

    create login form component inside users folder

---
+++ src/components/users/LoginInForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class LoginInForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default LoginInForm
