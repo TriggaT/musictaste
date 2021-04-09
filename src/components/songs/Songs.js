2a364eb194e84a716f1fd8543502772b90d60ebe
Trayshawn <traytwebb14@yahoo.com>
2021-04-09 13:34:54 -0500

    changed display of songs with box, gridlist, and list/listitem from material ui

--- src/components/songs/Songs.js
+++ src/components/songs/Songs.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom';
 import Song from "./Song"
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import GridList from "@material-ui/core/GridList";
+import { Box } from '@material-ui/core'
 
 class Songs extends Component {
     render() {
-        const { songs, match } = this.props;     
+        const { songs, match } = this.props; 
+        
+        function generate(element) {
+            return [1].map((value) =>
+            React.cloneElement(element, {
+                key: value,
+            }),
+            );
+        }
+            
         return (
             <div>
                 <h3>Songs</h3>
-                <ol>
-                    {songs.map(song => 
-                        <Song  key={song.id} song={song} />  )} 
-                </ol>
-                <NavLink to={`${match.url}/songs/new`}>Add New Song</NavLink>
-                
+                <GridList item xs={3} cols={5} spacing={2}  >
+                    {songs.map(song =>
+                    <div >
+                        <Box display="flex" width={500} height={50} >
+                            <Box m="auto">
+                                <List >
+                                {generate(
+                                    <ListItem  >
+                                        <ListItemText primary={`Title: ${song.title}`} secondary={`Artist: ${song.artist}`}/>
+                                    </ListItem>,
+                                )}
+                                </List>
+                            </Box>
+                        </Box>
+                    </div>
+                    )}
+                </GridList>
+                <NavLink to={`${match.url}/songs/new`}>Add New Song</NavLink> 
             </div>
         )
     }
