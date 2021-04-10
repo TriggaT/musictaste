6dfd1f57ae87cce3278276a4264ddf9e02f289f2
Trayshawn <traytwebb14@yahoo.com>
2021-04-10 01:14:02 -0500

    change new playlist link into material ui button

--- src/containers/PlaylistsContainer.js
+++ src/containers/PlaylistsContainer.js
@@ -8,6 +8,7 @@ import ListPlaylists from "../components/playlists/ListPlaylists"
 import {connect} from 'react-redux'
 import PlaylistForm from '../components/playlists/PlaylistForm';
 import { NavLink } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 
 
 export class PlaylistsContainer extends Component {
@@ -26,7 +27,9 @@ export class PlaylistsContainer extends Component {
                 <h3>Playlists</h3>
                 <ListPlaylists playlists={this.props.playlists} loading={this.props.loading} history={this.props.history} /> 
                 <br />
-                <NavLink  to="/playlists/new">New Playlist</NavLink>
+                <Button  onClick={() => this.props.history.push("/playlists/new")}variant="contained">
+                New Playlist
+                </Button> 
 
                 <Switch>
                 <Route path="/playlists/new" component={PlaylistForm} />
