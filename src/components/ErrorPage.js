74632491d41ac654a2b2e9ae6051314e25e97ae1
Trayshawn <traytwebb14@yahoo.com>
2021-04-11 00:49:42 -0500

    added info for error page inside material ui box

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import { Box } from '@material-ui/core'
 
 class ErrorPage extends Component {
     render() {
         return (
             <div>
-                Error Page
+                <br /> <br /> <br /> 
+                <Box m="auto" bgcolor="whitesmoke" alignItems="center" width={200} height={200} borderRadius="50%" >
+                <br/> <h2> Where's your MusicTaste? </h2> 
+                <p> Error 404 </p> 
+                <p> Page not found </p> 
+                    
+
+                </Box>
             </div>
         )
     }
