144c5708a5254b058b1f9ff538a6f2fe51b484d5
Trayshawn <traytwebb14@yahoo.com>
2021-04-11 01:49:56 -0500

    added info to contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,12 @@ export class Contact extends Component {
     render() {
         return (
             <div>
+                <Box m="auto" bgcolor="whitesmoke" alignItems="center" width={500} height={400} borderRadius="50%" >
+                    <br/> <h2> Contact </h2> 
+                    <p> Please feel free to reach out with any questions or feature request! </p>
+                    <p> If you just want to show love, I'd appreciate that too! </p> 
+                    <p> Email: traytwebb14@yahoo.com </p>
+                </Box> 
                 
             </div>
         )
