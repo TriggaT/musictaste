08fba21858c6c339cf57d4e1bfae450784942ce5
Trayshawn <traytwebb14@yahoo.com>
2021-04-11 01:55:43 -0500

    imported box into contact component and changed sizing for better display

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import { Box } from '@material-ui/core'
 
 export class Contact extends Component {
     render() {
         return (
             <div>
-                <Box m="auto" bgcolor="whitesmoke" alignItems="center" width={500} height={400} borderRadius="50%" >
+                <br /> <br /> <br /> 
+                <Box m="auto" bgcolor="whitesmoke" alignItems="center" width={400} height={250} borderRadius="50%" >
                     <br/> <h2> Contact </h2> 
                     <p> Please feel free to reach out with any questions or feature request! </p>
                     <p> If you just want to show love, I'd appreciate that too! </p> 
