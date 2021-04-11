256a3c3910fb6de133284254ebdfea414dc53f08
Trayshawn <traytwebb14@yahoo.com>
2021-04-11 01:38:42 -0500

    change sizing and display of about component

--- src/components/About.js
+++ src/components/About.js
@@ -5,13 +5,14 @@ export class About extends Component {
     render() {
         return (
             <div>
-                <Box m="auto" bgcolor="whitesmoke" alignItems="center" width={200} height={200} >
+                <br /> <br /> <br /> 
+                <Box m="auto" bgcolor="whitesmoke" alignItems="center" width={500} height={400} >
                     <br/> <h2> What's MusicTaste? </h2> 
                     <p> MusicTaste is a place for music lovers around the world to share in their love of music! </p> 
                     <p>Sometimes the vibe is so strong you can taste it! </p>
-                    <p> Just like when you're cooking up something good, you just want to share it someone else. </p>
+                    <p> Just like when you're cooking up something good, you just want to share it with someone else. </p>
                     <p> This is for artists, listeners, and anyone who just enjoys music. </p> 
-                    <p> Other tasters will let you know what taste your sound left in thier ears! </p>
+                    <p> Other tasters will let you know what taste your sound left in their ears! </p>
                     <p> You might even find your <i> music soulmate?!?!</i> </p>
                 </Box> 
                 
