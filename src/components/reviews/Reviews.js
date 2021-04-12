972759e8f21051d6668ab92d5ac52b51c65ef1bc
Trayshawn <traytwebb14@yahoo.com>
2021-04-11 23:49:13 -0500

    compare current user to playlist user to prevent users from reviewing thier own playlist

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -8,8 +8,10 @@ export class Reviews extends Component {
     render() {
         const { reviews, match, history, currentUser, playlistUserId } = this.props;
 
+        console.log(playlistUserId)
+
         const renderReviewButton = () => {
-            if(JSON.stringify(currentUser) !== "{}"){
+            if(JSON.stringify(currentUser) !== "{}" && currentUser.id !== playlistUserId){
                 return  (
                     <Button  onClick={() => history.push(`${match.url}/reviews/new`)} variant="contained">
                     Add Your Review
