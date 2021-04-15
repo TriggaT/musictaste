5a5cb88f82f76a4ea5ea94b16149a01e0fe48453
Trayshawn <traytwebb14@yahoo.com>
2021-04-15 12:46:59 -0500

    removed unneeded imports and move imports from same location together in navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
-import { NavLink } from 'react-router-dom';
+import { NavLink,  withRouter } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import { withRouter} from "react-router-dom"
 import {connect} from 'react-redux'
 import {logOutUser, getPlaylists, userLoggedIn} from '../actions/index'
 import { LogButton } from './users/LogButton';
