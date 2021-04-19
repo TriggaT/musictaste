213e7b130af7bfd83be814e10728cc31d4244788
Trayshawn <traytwebb14@yahoo.com>
2021-04-19 00:26:22 -0500

    removed loading and not loading reducer actions from getReviews and getSongs

--- src/actions/index.js
+++ src/actions/index.js
@@ -134,11 +134,9 @@ export const addPlaylist = (playlist, id, history) => {
 
 export const getSongs = () => {
     return dispatch => {
-        dispatch({ type: 'LOADING'})
         fetch("/songs")
         .then(r => r.json())
         .then(songs => {
-            dispatch({ type: 'NOT_LOADING'})
             dispatch({type: 'SET_SONGS', songs})}
         )
     }
@@ -170,11 +168,9 @@ export const addSong = (song, id, history) => {
 
 export const getReviews = () => {
     return dispatch => {
-        dispatch({ type: 'LOADING'})
         fetch("/reviews")
         .then(r => r.json())
         .then(reviews => {
-            dispatch({ type: 'NOT_LOADING'})
             dispatch({type: 'GET_REVIEWS', reviews})}
         )
     }
